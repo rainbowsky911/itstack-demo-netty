ac17a2938456f20767287aa06ddb14d992b1a91b
rainbowsky911 <zdwbmw@163.com>
2021-05-07 21:21:38 +0800

    Netty的心跳机制

---
+++ HeartBeat/src/main/java/com/example/heartbeat/heartbeat/client/HeartBeatsClientChannelInitializer.java
@@ -0,0 +1,23 @@
+package com.example.heartbeat.heartbeat.client;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+public class HeartBeatsClientChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
+
+        pipeline.addLast("handler", new IdleStateHandler(0, 3, 0, TimeUnit.SECONDS));
+        pipeline.addLast("decoder", new StringDecoder());
+        pipeline.addLast("encoder", new StringEncoder());
+        pipeline.addLast(new HeartBeatClientHandler());
+    }
+}
---
+++ HeartBeat/src/main/java/com/example/heartbeat/heartbeat/server/HeartBeatServerChannelInitializer.java
@@ -0,0 +1,23 @@
+package com.example.heartbeat.heartbeat.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+public class HeartBeatServerChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
+
+        //读时间设置3秒,写和读写超时设置为0
+        pipeline.addLast("handler",new IdleStateHandler(3, 0, 0, TimeUnit.SECONDS));
+        pipeline.addLast("decoder", new StringDecoder());
+        pipeline.addLast("encoder", new StringEncoder());
+        pipeline.addLast(new HeartBeatServerHandler());
+    }
+}
